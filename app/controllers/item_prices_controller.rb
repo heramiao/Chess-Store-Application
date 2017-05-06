b40ff360b78ff4e484c37ecb4701bcab04f974c9
Hera Miao <heram@andrew.cmu.edu>
2017-05-05 23:58:53 -0400

    attempted to add ajax

--- app/controllers/item_prices_controller.rb
+++ app/controllers/item_prices_controller.rb
@@ -8,16 +8,23 @@ class ItemPricesController < ApplicationController
 
   def new
     @item_price = ItemPrice.new
+    @item = @item_price.item
   end
 
   def create
     @item_price = ItemPrice.new(item_price_params)
     @item_price.start_date = Date.current
-    if @item_price.save
-      @item = @item_price.item
-      redirect_to item_path(@item), notice: "Changed the price of #{@item.name}."
-    else
-      render action: 'new'
+
+    respond_to do |format|
+      if @item_price.save
+        format.html { redirect_to @item_price, notice: 'Item Price was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @item_price }
+        @item = @item_price.item
+        @price_history = @item.item_prices.chronological.to_a
+        format.js 
+      else
+        render action: 'new'
+      end
     end
   end
 
