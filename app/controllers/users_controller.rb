c5a76ca7ed720e2633e6c0f164a5f770282441da
Hera Miao <heram@andrew.cmu.edu>
2017-05-08 15:29:55 -0400

    altered a lot of things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save
+        if @user.save!
             session[:user_id] = @user.id
             redirect_to home_path, notice: "You have successfully signed up!"
         else
