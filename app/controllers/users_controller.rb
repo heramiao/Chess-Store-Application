c060a903eb9ef6062779db6bc84e40e10edf3846
Hera Miao <heram@andrew.cmu.edu>
2017-05-08 16:18:43 -0400

    i need to study for the exam tomorrow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,12 +45,12 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save!
+        if @user.save
             session[:user_id] = @user.id
             redirect_to home_path, notice: "You have successfully signed up!"
         else
             flash[:error] = "This user could not be created."
-            render "new"
+            render action: 'new'
         end
     end
 
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
                 flash[:notice] = "#{@user.proper_name} has been updated."
                 redirect_to @user
             else
-                render :action => 'edit'
+                render action: 'edit'
             end
         else
             redirect_to home_path, notice: "Do not have access to update user'"
