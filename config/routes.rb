5b2bbd253848f45ae64adf4111b5ba47efdc52d5
Hera Miao <heram@andrew.cmu.edu>
2017-05-10 06:48:25 -0400

    added error page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   # Routes for main resources
   resources :items
   resources :purchases
@@ -42,5 +46,9 @@ Rails.application.routes.draw do
 
   # Random routes
   get 'order_item/ship_item/:id' => 'order_items#ship_item', :as => :ship_item
+
+  # Error routes
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
 end
